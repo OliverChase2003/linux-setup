1e998e196de1be8a548b1ffe8dded5018774543a
OliverChase <82888216+OliverChase2003@users.noreply.github.com>
2024-11-25 20:16:55 +0800

    init
    init

---
+++ replace.bash
@@ -0,0 +1,14 @@
+## back up
+### nvim
+zip ~/.config/nvim ~/.config/nvim-backup.zip
+### fish
+zip ~/.config/fish ~/.config/fish-backup.zip
+### ranger
+
+## replace
+### nvim
+cp ./.config/nvim ~/.config/nvim -r
+### fish
+cp ./.config/fish ~/.config/fish -r
+### ranger
+
---
+++ sync.bash
@@ -0,0 +1,11 @@
+## nvim
+cp -r ~/.config/nvim ./.config/
+
+## bash
+cp ~/.bash_profile ./.bash_profile
+cp ~/.bashrc ./.bashrc
+
+## fish
+cp ~/.config/fish/config.fish ./.config/fish/config.fish
+
+
